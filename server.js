2e3d1fbf7de318ac30fd17d13dc7398838ed968c
W53 <web5xcss3@gmail.com>
2026-04-22 05:18:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors()); // 👈 resolve tudo de CORS
+
 
 const PORT = process.env.PORT || 3000;
 
