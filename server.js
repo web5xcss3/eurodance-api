bdb44b7957b7129c687439702c8af388a180254f
W53 <web5xcss3@gmail.com>
2026-04-22 05:04:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 const express = require('express');
-
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// 🔥 CORS LIBERADO
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.get('/youtube', async (req, res) => {
 
-    const q = req.query.q || 'eurodance';
-    const key = process.env.YOUTUBE_KEY;
-
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&maxResults=12&key=${key}`;
+    const query = req.query.q || 'eurodance';
+    const API_KEY = process.env.YOUTUBE_KEY;
 
     try {
-        const response = await fetch(url);
+
+        const response = await fetch(
+            `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=12&key=${API_KEY}`
+        );
+
         const data = await response.json();
 
         res.json(data);
 
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 
 });
 
-console.log("KEY:", process.env.YOUTUBE_KEY);
-
-app.listen(3000, () => console.log('API rodando'));
+app.listen(PORT, () => {
+    console.log('API rodando');
+});
